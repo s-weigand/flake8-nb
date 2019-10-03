709ca0e317190f72ec6a44184e4d7215019cdd67
s-weigand <s.weigand.phy@gmail.com>
2019-10-03 23:54:22 +0200

    fix style issues

--- flake8_nb/__main__.py
+++ flake8_nb/__main__.py
@@ -1,18 +1,22 @@
 """Command-line implementation of flake8."""
 import sys
-from typing import List
+from typing import List, Optional
 
 from flake8_nb.flake8_integration import Flake8NbApplication
 
 from flake8_nb import FLAKE8_VERSION_TUPLE
 
 
-def main(argv=None):
-    # type: (Optional[List[str]]) -> None
-    """Execute the main bit of the application.
+def main(argv: Optional[List[str]] = None) -> None:
+    """
+    Execute the main bit of the application.
     This handles the creation of an instance of :class:`Application`, runs it,
     and then exits the application.
-    :param list argv:
+
+
+    Parameters
+    ----------
+    argv: List[str], optional
         The arguments to be passed to the application for parsing.
     """
     # TODO: remove compat after flake8>3.7.8 release
