124cad1c78aef64404d605d255cd67312b9fbcde
s-weigand <s.weigand.phy@gmail.com>
2019-10-04 21:19:47 +0200

    added the symlinks to readme and history in the docs to git ignore
    also added

---
+++ docs/refresh_symlinks_windows.sh
@@ -0,0 +1,7 @@
+# Run this in gitbash with administrator rights, if you are on windows
+# and the docs won't build properly.
+export MSYS=winsymlinks:nativestrict
+rm readme.md
+rm history.md
+ln -s ../README.md readme.md
+ln -s ../HISTORY.md history.md
