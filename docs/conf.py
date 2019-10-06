1252182d6bdf551b920e54875510c25cc13b5cfc
s-weigand <s.weigand.phy@gmail.com>
2019-10-06 19:41:29 +0200

    fixed icon size of sphinx-copybutton in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "recommonmark",
+    "sphinx_copybutton",
 ]
 
 autoclass_content = "both"
@@ -109,7 +110,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
@@ -173,3 +174,6 @@ texinfo_documents = [
     )
 ]
 
+
+def setup(app):
+    app.add_stylesheet("css/copy-btn-overwrite.css")
