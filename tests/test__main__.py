7b7050814375bb76c025746d0e7f0911099a7ad8
s-weigand <s.weigand.phy@gmail.com>
2019-10-18 20:23:15 +0200

    removed subprocess test from tests for main

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -13,50 +13,26 @@ from . import TEST_NOTEBOOK_BASE_PATH
 
 
 @pytest.mark.parametrize("keep_intermediate", [True, False])
-def test_integration_system_call(keep_intermediate: bool):
-    argv = ["flake8_nb", TEST_NOTEBOOK_BASE_PATH]
-    if keep_intermediate:
-        argv.append("--keep-parsed-notebooks")
-    process = subprocess.Popen(
-        argv, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    stdout_value, stderr_value = process.communicate()
-    result_list = stdout_value.replace(b"\r", b"").split(b"\n")
-    expected_result_path = os.path.join(
-        os.path.dirname(__file__), "data", "expected_output.txt"
-    )
-    with open(expected_result_path, "rb") as result_file:
-        expected_result_list = result_file.readlines()
-    for result, expected_result in zip(result_list, expected_result_list):
-        assert result.endswith(expected_result.rstrip(b"\n"))
-
-    if keep_intermediate:
-        temp_path = re.search(rb"present at:\s+(.+?)\s+", stderr_value, re.DOTALL)
-        assert os.path.exists(temp_path.group(1))
-        NotebookParser.temp_path = temp_path.group(1)
-        NotebookParser.clean_up()
-
-
-@pytest.mark.parametrize("keep_intermediate", [True, False])
 def test_run_main(capsys, keep_intermediate: bool):
     argv = ["flake8_nb", TEST_NOTEBOOK_BASE_PATH]
     if keep_intermediate:
         argv.append("--keep-parsed-notebooks")
-    try:
+    with pytest.raises(SystemExit):
         with pytest.warns(InvalidNotebookWarning):
             main(argv)
-    except SystemExit:
-        pass
     captured = capsys.readouterr()
     result_output = captured.out
     result_list = result_output.replace("\r", "").split("\n")
+    result_list.remove("")
     expected_result_path = os.path.join(
         os.path.dirname(__file__), "data", "expected_output.txt"
     )
     with open(expected_result_path, "r") as result_file:
         expected_result_list = result_file.readlines()
-    for result, expected_result in zip(result_list, expected_result_list):
-        assert result.endswith(expected_result.rstrip("\n"))
+    for expected_result in expected_result_list:
+        assert any(
+            [result.endswith(expected_result.rstrip("\n")) for result in result_list]
+        )
 
     if keep_intermediate:
         assert os.path.exists(NotebookParser.temp_path)
