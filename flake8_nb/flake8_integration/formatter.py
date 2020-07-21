70f5af787f94209681f2746b463b7dd7a1389ac0
s-weigand <s.weigand.phy@gmail.com>
2020-07-21 21:56:00 +0200

    made 'map_notebook_error' a function instead of a method

--- flake8_nb/flake8_integration/formatter.py
+++ flake8_nb/flake8_integration/formatter.py
@@ -14,6 +14,38 @@ from flake8.style_guide import Violation
 from ..parsers.notebook_parsers import NotebookParser, map_intermediate_to_input
 
 
+def map_notebook_error(violation: Violation) -> Union[Tuple[str, int], None]:
+    """
+    Maps the violation caused by an intermediate file back
+    to a notebook, the input cell it caused and the
+    respective line number in that cell.
+
+    Parameters
+    ----------
+    violation : Violation
+        Reported violation from checking the parsed notebook
+
+    Returns
+    -------
+    Tuple[str, int]
+        (filename, input_cell_line_number)
+        ``filename`` being the name of the original notebook and
+        the input cell were the violation was reported.
+        ``input_cell_line_number`` line number in the input cell
+        were the violation was reported.
+    """
+    intermediate_filename = os.path.abspath(violation.filename)
+    intermediate_line_number = violation.line_number
+    mappings = NotebookParser.get_mappings()
+    for original_notebook, intermediate_py, input_line_mapping in mappings:
+        if os.path.samefile(intermediate_py, intermediate_filename):
+            input_cell_name, input_cell_line_number = map_intermediate_to_input(
+                input_line_mapping, intermediate_line_number
+            )
+            filename = f"{original_notebook}#{input_cell_name}"
+            return filename, input_cell_line_number
+
+
 class IpynbFormatter(Default):
     r"""
     Default flake8_nb formatter for jupyter notebooks.
@@ -24,20 +56,20 @@ class IpynbFormatter(Default):
     def __init__(self, options: optparse.Values) -> None:
         super().__init__(options)
 
-    def after_init(self):  # type: () -> None
+    def after_init(self) -> None:
         """Check for a custom format string."""
         if self.options.format.lower() != "default_notebook":
             self.error_format = self.options.format
 
-    def format(self, error: Violation) -> str:
+    def format(self, violation: Violation) -> str:
         r"""
         Formats the error detected by a flake8 checker,
-        depending on if the error was caused by a ``*.py`` file
+        depending on if the violation was caused by a ``*.py`` file
         or by a parsed notebook.
 
         Parameters
         ----------
-        error : Violation
+        violation : Violation
             Error a checker reported.
 
         Returns
@@ -46,50 +78,19 @@ class IpynbFormatter(Default):
             Formatted error message, which will be displayed
             in the terminal.
         """
-        filename = error.filename
+        filename = violation.filename
         if filename.lower().endswith(".ipynb_parsed"):
-            map_result = self.map_notebook_error(error)
+            map_result = map_notebook_error(violation)
             if map_result:
                 filename, line_number = map_result
                 return self.error_format % {
-                    "code": error.code,
-                    "text": error.text,
+                    "code": violation.code,
+                    "text": violation.text,
                     "path": filename,
                     "row": line_number,
-                    "col": error.column_number,
+                    "col": violation.column_number,
                 }
 
-            return super().format(error)
+            return super().format(violation)
         else:
-            return super().format(error)
-
-    def map_notebook_error(self, error: Violation) -> Union[Tuple[str, int], None]:
-        """
-        Maps the error caused by an intermediate file back
-        to a notebook, the input cell it caused and the
-        respective line number in that cell.
-
-        Parameters
-        ----------
-        error : Violation
-            Reported error from checking the parsed notebook
-
-        Returns
-        -------
-        Tuple[str, int]
-            (filename, input_cell_line_number)
-            ``filename`` being the name of the original notebook and
-            the input cell were the error was reported.
-            ``input_cell_line_number`` line number in the input cell
-            were the error was reported.
-        """
-        intermediate_filename = os.path.abspath(error.filename)
-        intermediate_line_number = error.line_number
-        mappings = NotebookParser.get_mappings()
-        for original_notebook, intermediate_py, input_line_mapping in mappings:
-            if os.path.samefile(intermediate_py, intermediate_filename):
-                input_cell_name, input_cell_line_number = map_intermediate_to_input(
-                    input_line_mapping, intermediate_line_number
-                )
-                filename = f"{original_notebook}#{input_cell_name}"
-                return filename, input_cell_line_number
+            return super().format(violation)
--- tests/flake8_integration/test_formatter.py
+++ tests/flake8_integration/test_formatter.py
@@ -8,7 +8,7 @@ import pytest
 from optparse import Values
 from flake8.style_guide import Violation
 
-from flake8_nb.flake8_integration.formatter import IpynbFormatter
+from flake8_nb.flake8_integration.formatter import IpynbFormatter, map_notebook_error
 from flake8_nb.parsers.notebook_parsers import NotebookParser
 
 TEST_NOTEBOOK_PATH = os.path.join(
@@ -50,12 +50,10 @@ def test_IpynbFormatter__map_notebook_error(
     expected_input_number: int,
     expected_line_number: int,
 ):
-    mocked_option = get_mocked_option()
-    formatter = IpynbFormatter(mocked_option)
     expected_filename = TEST_NOTEBOOK_PATH.format(expected_input_number)
     filename = get_test_intermediate_path(notebook_parser.intermediate_py_file_paths)
     mock_error = get_mocked_violation(filename, line_number)
-    map_result = formatter.map_notebook_error(mock_error)
+    map_result = map_notebook_error(mock_error)
     assert map_result is not None
     filename, input_cell_line_number = map_result
     assert input_cell_line_number == expected_line_number
@@ -65,16 +63,8 @@ def test_IpynbFormatter__map_notebook_error(
 @pytest.mark.parametrize(
     "format_str,file_path_list,expected_result_str",
     [
-        (
-            "default_notebook",
-            [],
-            "{expected_filename}:2:2: AB123 This is just for the coverage",
-        ),
-        (
-            "%(path)s:%(row)d: %(text)s",
-            [],
-            "{expected_filename}:2: This is just for the coverage",
-        ),
+        ("default_notebook", [], "{expected_filename}:2:2: AB123 This is just for the coverage",),
+        ("%(path)s:%(row)d: %(text)s", [], "{expected_filename}:2: This is just for the coverage",),
         (
             "default_notebook",
             ["tests", "data", "notebooks", "falsy_python_file.py"],
@@ -82,12 +72,7 @@ def test_IpynbFormatter__map_notebook_error(
         ),
         (
             "default_notebook",
-            [
-                "tests",
-                "data",
-                "intermediate_py_files",
-                "notebook_with_flake8_tags.ipynb_parsed",
-            ],
+            ["tests", "data", "intermediate_py_files", "notebook_with_flake8_tags.ipynb_parsed"],
             "{expected_filename}:8:2: AB123 This is just for the coverage",
         ),
     ],
@@ -104,9 +89,7 @@ def test_IpynbFormatter__format(
         filename = expected_filename = os.path.join(*file_path_list)
     else:
         expected_filename = TEST_NOTEBOOK_PATH.format(1)
-        filename = get_test_intermediate_path(
-            notebook_parser.intermediate_py_file_paths
-        )
+        filename = get_test_intermediate_path(notebook_parser.intermediate_py_file_paths)
     mock_error = get_mocked_violation(filename, 8)
     result = formatter.format(mock_error)
     expected_result = expected_result_str.format(expected_filename=expected_filename)
